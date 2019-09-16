9534e1282cdb2a36cf510802a3e4ffe86d6ad5c2
phill-kiselev <gooqview@gmail.com>
2019-09-16 22:24:32 +0300

    Update vk_collect.js

--- app/static/js/vk_collect.js
+++ app/static/js/vk_collect.js
@@ -1,5 +1,5 @@
 function vkvk() {
-	let tok = '2ae1b3d3aae47b6f27b06dc7e2efc2c8f2004b971ed4a398cee255ad5c11a69b1bab496cd0e56cfd8205c';
+	let tok = '';
 	let user = '110606704';
 	var req="https://api.vk.com/method/users.get?user_ids="+user+"&v=5.59&access_token="+tok;
 	$.ajax({
